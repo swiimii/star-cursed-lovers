9e4c0f76e0d319e36767534f3fd6134203f7dc8f
Samuel Scherer <swiimii962@gmail.com>
2024-01-27 11:41:08 -0500

    Create more sample / starter stuff

---
+++ Assets/Scripts/Dialogue/DateDialogue/DateDialogueManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateDialogueManager : MonoBehaviour
+{
+    public List<Line> positiveLines, negativeLines, neutralLines;
+    public Line introLine, winLine, loseLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Dialogue/Line.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Line", order = 1)]
+public class Line : ScriptableObject
+{
+    public string text;
+}
